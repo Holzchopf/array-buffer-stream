86f8612584ba1840d29b9fe09419df6f57b34a93
Holzchopf <oli4stuker@hotmail.com>
2023-11-23 15:23:52 +0100

    ADDED reading signed numbers

--- src/array-buffer-stream.ts
+++ src/array-buffer-stream.ts
@@ -87,6 +87,68 @@ export class ArrayBufferStream {
     } while (true)
     return num
   }
+
+  /**
+   * Reads the next byte as signed 8 bit integer and returns it as number.
+   */
+  readInt8() {
+    const view = new DataView(this.buffer, this.cursor, 1)
+    this.cursor += 1
+    return view.getInt8(0)
+  }
+
+  /**
+   * Reads the next bytes as signed 16 bit integer and returns it as number.
+   * @param littleEndian If true, a little-endian value should be read.
+   */
+  readInt16(littleEndian?: boolean | undefined) {
+    const view = new DataView(this.buffer, this.cursor, 2)
+    this.cursor += 2
+    return view.getInt16(0, littleEndian)
+  }
+  
+  /**
+   * Reads the next bytes as signed 32 bit integer and returns it as number.
+   * @param littleEndian If true, a little-endian value should be read.
+   */
+  readInt32(littleEndian?: boolean | undefined) {
+    const view = new DataView(this.buffer, this.cursor, 4)
+    this.cursor += 4
+    return view.getInt32(0, littleEndian)
+  }
+
+  /**
+   * Reads the next bytes as signed 64 bit integer and returns it as bigint.
+   * @param littleEndian If true, a little-endian value should be read.
+   */
+  readInt64(littleEndian?: boolean | undefined) {
+    const view = new DataView(this.buffer, this.cursor, 8)
+    this.cursor += 8
+    return view.getBigInt64(0, littleEndian)
+  }
+
+  /**
+   * Reads the next bytes as signed LEB128 value and returns it as number.
+   */
+  readLeb128() {
+    let num = 0
+    let shift = 0
+    do {
+      const byte = this.readUint8()
+      // append next 7 bits to number
+      num |= (byte & 0x7f) << shift
+      shift += 7
+      // stop once highest bit is 0
+      if (!(byte & 0x80)) {
+        // sign bit is second high order bit
+        if (shift < 32 && (byte & 0x40)) {
+          num |= (~0 << shift)
+        }
+        break
+      }
+    } while (true)
+    return num
+  }
   
   /**
    * Reads a number of bytes as ascii string and returns it as string.
@@ -110,4 +172,6 @@ export class ArrayBufferStream {
     const decoder = new TextDecoder(littleEndian ? 'utf-16le' : 'utf-16be')
     return decoder.decode(view)
   }
+
+
 }
